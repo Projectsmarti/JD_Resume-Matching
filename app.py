797c7314a7751f50b12fc8fd40284426d8eed1a8
Projectsmarti <151812630+Projectsmarti@users.noreply.github.com>
2023-11-27 17:48:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ else:
             st.write(f"SKILLS REQUIRED: {jd_skills}")
             st.write(f"EXPERIENCE REQUIRED: {jd_experience}")
 
-        resume_data = pd.read_csv("Resume_Parsed_Sample_v4_with_exp.csv")
+        resume_data = pd.read_csv("Resume_Parsed_Sample_v4_with_exp_refurb.csv")
 
         if st.button("Matched Resumes"):
             jd_full_text, jd_skills, jd_experience = get_jd_skills_and_exp(jd_full_text)
@@ -221,4 +221,3 @@ else:
                                         'Additional_skills', 'Phone Number', 'Email id']]
             top_5_matches = top_5_matches.head(5)
             top_5_matches
-
